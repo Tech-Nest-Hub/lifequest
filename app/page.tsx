e3834479e986ac8acc60e4a86243c79c97eb24d2
Ghost9841 <asubedi778@gmail.com>
2025-11-04 17:47:24 +0545

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,15 @@ import { Footer } from "@/components/landingpageui/footer";
 import { Hero } from "@/components/landingpageui/herosection";
 import { HowItWorks } from "@/components/landingpageui/how-it-works";
 import { Navbar } from "@/components/landingpageui/navbar";
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-linear-to-b from-background to-slate-50">
+    <main className="min-h-screen bg-black text-white overflow-hidden">
       <Navbar />
       <Hero />
       <Features />
       <HowItWorks />
-      <CTA/>
+      <CTA />
       <Footer />
     </main>
   );
